217eb63542090cde263ca56b282bf9171f6d21ec
bill1997603 <fan1997603@vip.qq.com>
2020-09-21 14:32:56 +0800

    feat: users index order by mulit columns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(job_status: :asc, job_title: :desc, job_status: :asc, age: :desc)
   end
 
   # GET /users/1
