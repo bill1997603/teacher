c6859c450d2c8ec65ad32a2f7c0a67c5ea64fddb
bill1997603 <fan1997603@vip.qq.com>
2020-09-21 15:57:42 +0800

    feat: validate user columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-  enum job_type: { '实习教师': 0, '正式教师': 1, '退休教师': 2 }
+  validates :name, presence: true
+  validates :phone, presence: true, length: { is: 11}
+
+  validates_format_of :phone, with:  /\d[0-9]\)*\z/
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, allow_blank: true
+
+  enum job_type: { '男': 0, '女': 1 }
+  enum job_type: { '正式教师': 0, '退休教师': 1, '实习教师': 2 }
   enum job_status: { '正常': 0, '休假中': 1, '已离职': 2 }
   enum job_title: { '无': 0, '初级': 1, '中级': 2, '高级': 3 }
 end
